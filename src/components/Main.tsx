433176e6816631518ead45a1c7975c88c18b0209
sylene <sylene.manusset@gmail.com>
2020-10-05 18:41:49 +0200

    [WIP] First try: add three states representing salary, expenses and remaining savings

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Income from "./Income";
 
 const Main = () => {
+  const [value, setValue] = useState({
+    income: 1500,
+    expenses: 0,
+  });
+
+  const [saving, setSaving] = useState(value.income);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
   return (
     <main className="container" style={{ flex: 1 }}>
-      <div className="row text-center mt-4">
+      <div className="row justify-content-center text-center mt-4">
         <div className="col-lg-12">
-          <Income incomeName="Salary" incomeValue={1500} />
+          <Income incomeName="Salary" incomeValue={value.income} />
+        </div>
+        <div className="col-lg-6">
+          <input
+            className="form-control"
+            name="expenses"
+            type="number"
+            placeholder="0"
+            onChange={handleChange}
+          ></input>
+          <button
+            type="button"
+            onClick={() => {
+              setSaving(saving - value.expenses);
+            }}
+            className="btn btn-light float-right mt-2"
+          >
+            Calculate
+          </button>
+        </div>
+        <div className="col-lg-12">
+          <Income incomeName="Saving" incomeValue={saving} />
         </div>
       </div>
     </main>
